cad846eef8a7666fc10f51494a8de9827796212b
qmastermoo <cpsaicontact@gmail.com>
2023-01-16 01:16:58 -0500

    varying structure, but not working

--- salt_shaker/image_actions/swirl.py
+++ salt_shaker/image_actions/swirl.py
@@ -42,3 +42,30 @@ class VariableSwirl(ImageAction):
             processed_images.append(img)
 
         return ImageAction._unlistify_output(processed_images)
+
+class VaryingVariableSwirl(ImageAction):
+    def __init__(self):
+        super().__init__()
+
+    def process(self, image_input: Image | list[Image], depth: int) -> Image | list[Image]:
+        """
+        if multiple images are passed in, array with size len(image_input)*depth return
+        index with [image_idx * depth + depth_idx]
+        """
+        if not isinstance(depth, int):
+            raise Exception(f"depth is not valid int [{depth}]")
+        if depth < 0:
+            # todo - make fun thing with negatives
+            raise Exception(f"depth cannot be negative [{depth}]")
+
+        image_input = ImageAction._listify_input(image_input)
+        
+        processed_images: list[Image] = []
+        variable_swirl = VariableSwirl()
+        for img in image_input:
+            for i in range(0, depth):
+                img = variable_swirl.process(img, i)
+                processed_images.append(img)
+
+        # _unlistify doesn't do much here, i guess if we have 1 img 1 depth
+        return ImageAction._unlistify_output(processed_images)
--- sample_run.py
+++ sample_run.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 from salt_shaker.image import Image
-from salt_shaker.image_actions.swirl import BasicSwirl, VariableSwirl
+from salt_shaker.image_actions.swirl import BasicSwirl, VariableSwirl, VaryingVariableSwirl
 
 orange_image = Image.create_from_file("./sample_data/orange.png")
 
@@ -14,3 +14,13 @@ output_image.write_to_file(f"./sample_output/orange_basic_swirl.png")
 variable_swirl = VariableSwirl()
 output_image_depth_5 = variable_swirl.process(orange_image, 5)
 output_image_depth_5.write_to_file(f"./sample_output/orange_variable_swirl_depth_5.png")
+output_image_depth_10 = variable_swirl.process(orange_image, 10)
+output_image_depth_10.write_to_file(f"./sample_output/orange_variable_swirl_depth_10.png")
+
+# this will take a while
+varying_variable_swirl = VaryingVariableSwirl()
+output_varying_25 = varying_variable_swirl.process(orange_image, 25)
+for i, img in enumerate(output_varying_25):
+    if not (i % 5 == 0):
+        continue
+    img.write_to_file(f"./sample_output/orange_varying_variable_swirl_depth_{i}.png")
