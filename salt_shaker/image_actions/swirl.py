d0c0ac69ca4f5773c037a645808c8ff1ee983065
qmastermoo <cpsaicontact@gmail.com>
2023-01-16 01:00:47 -0500

    variable swirl

--- salt_shaker/image_actions/swirl.py
+++ salt_shaker/image_actions/swirl.py
@@ -16,4 +16,29 @@ class BasicSwirl(ImageAction):
             img.image_data = transform.swirl(img.image_data)
             processed_images.append(img)
 
-        return processed_images[0] if len(processed_images) == 1 else processed_images
+        return ImageAction._unlistify_output(processed_images)
+
+
+class VariableSwirl(ImageAction):
+    def __init__(self):
+        super().__init__()
+
+    def process(
+        self, image_input: Image | list[Image], depth: int
+    ) -> Image | list[Image]:
+        if not isinstance(depth, int):
+            raise Exception(f"depth is not valid int [{depth}]")
+        if depth < 0:
+            # todo - make fun thing with negatives
+            raise Exception(f"depth cannot be negative [{depth}]")
+
+        image_input = ImageAction._listify_input(image_input)
+
+        processed_images: list[Image] = []
+        basic_swirl = BasicSwirl()
+        for img in image_input:
+            for i in range(0, depth):
+                img = basic_swirl.process(img)
+            processed_images.append(img)
+
+        return ImageAction._unlistify_output(processed_images)
