9f2ebb5aaec31a7a7eb51739bcd99730c03e5daa
qmastermoo <cpsaicontact@gmail.com>
2023-01-16 01:00:59 -0500

    variable swirl

--- sample_run.py
+++ sample_run.py
@@ -1,11 +1,16 @@
 from datetime import datetime
 from salt_shaker.image import Image
-from salt_shaker.image_actions.swirl import BasicSwirl
+from salt_shaker.image_actions.swirl import BasicSwirl, VariableSwirl
 
-gnome_image = Image.create_from_file("./sample_data/orange.png")
+orange_image = Image.create_from_file("./sample_data/orange.png")
 
-swirl_basic = BasicSwirl()
-output_image = swirl_basic.process(gnome_image)
+basic_swirl = BasicSwirl()
+output_image = basic_swirl.process(orange_image)
 
-#filetime = datetime.now().strftime("%Y%m%d-%H%M%S")
-output_image.write_to_file(f'./sample_output/orange_basic_swirl.png')
+# filetime = datetime.now().strftime("%Y%m%d-%H%M%S")
+output_image.write_to_file(f"./sample_output/orange_basic_swirl.png")
+
+
+variable_swirl = VariableSwirl()
+output_image_depth_5 = variable_swirl.process(orange_image, 5)
+output_image_depth_5.write_to_file(f"./sample_output/orange_variable_swirl_depth_5.png")
