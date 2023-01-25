66391bcee708325c4599ee39ff491d8d59aa7c1b
qmastermoo <cpsaicontact@gmail.com>
2023-01-25 00:21:23 -0500

    horizontal_shift working

--- salt_shaker/image_actions/translate.py
+++ salt_shaker/image_actions/translate.py
@@ -13,26 +13,47 @@ class Translate(ChainImageAction):
     def __init__(self):
         super().__init__()
 
-    def process(self, input_batch: FrameBatch, horizontal_pixels=0, vertical_pixels=0) -> FrameBatch:
+    def process(self, input_batch: FrameBatch, horizontal_shift_px=0, vertical_shift_px=0) -> FrameBatch:
         """
         translate image up down left right
         """
-        if horizontal_pixels == 0 and vertical_pixels == 0:
+        # todo type checking and errors
+
+        if horizontal_shift_px == 0 and vertical_shift_px == 0:
+            # todo clone
             return input_batch
 
         output_batch = FrameBatch()
         for frame in input_batch.frames:
             frame = frame.clone()
 
-            arr_builder_iter = (Translate._empty_frame for _ in range(30))
-            arr_dtype = np.dtype((frame.data_arr[1].dtype, 4))
-            empty_frame_vertical_arr = np.fromiter(arr_builder_iter, arr_dtype)
+            # handle horizontal shifts
+            if horizontal_shift_px != 0:
+                # create empty array
+                arr_builder_iter = (Translate._empty_frame for _ in range(abs(horizontal_shift_px)))
+                arr_dtype = np.dtype((frame.data_arr[1].dtype, 4))
+                empty_frame_vertical_arr = np.fromiter(arr_builder_iter, arr_dtype)
 
+                for h_idx in range(frame.height):
+                    a = empty_frame_vertical_arr
+                    b = frame.data_arr[h_idx]
 
-            for h_idx in range(frame.height):
-                a = empty_frame_vertical_arr
-                b = frame.data_arr[h_idx]
-                frame.data_arr[h_idx] = np.concatenate((a, b), axis=0)[:frame.width]
+                    intermediate_arr = frame.data_arr[h_idx]
+                    if horizontal_shift_px > 0:
+                        # shorten array
+                        intermediate_arr = intermediate_arr[:frame.width-horizontal_shift_px]
+                        # build it back up
+                        intermediate_arr = np.concatenate((empty_frame_vertical_arr, intermediate_arr), axis=0)
+                    else:
+                        # shorten array
+                        intermediate_arr = intermediate_arr[abs(horizontal_shift_px):]
+                        # build it back up
+                        intermediate_arr = np.concatenate((intermediate_arr, empty_frame_vertical_arr), axis=0)
+
+                    frame.data_arr[h_idx] = intermediate_arr
+
+                    # combine arrays and pickup only pixels we want
+
 
 
             # img_nd_arr = transform.swirl(frame.as_3d_ndarray())
@@ -40,5 +61,3 @@ class Translate(ChainImageAction):
 
 
         return output_batch
-
-
