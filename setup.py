a35d1cd4cef527b9fff6d6ada98efae7cb808172
qmastermoo <cpsaicontact@gmail.com>
2023-01-30 01:42:58 -0500

    add setup.py for pyp

---
+++ setup.py
@@ -0,0 +1,19 @@
+# only needed for pypi
+import os.path
+
+from setuptools import setup
+
+
+
+if __name__ == '__main__'
+
+    readme_path = os.path.join(os.path.dirname(__file__), 'README.md')
+    with open(readme_path, encoding="utf-8") as f:
+        # todo use md comments to make pypi friendly description
+        readme = f.read()
+
+
+    setup(
+        long_description=readme,
+        long_description_content_type='text/markdown',
+    )
