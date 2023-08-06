030b38274bb339c8bfec268f358499c081cafd31
qmastermoo <cpsaicontact@gmail.com>
2023-08-06 19:47:19 -0400

    test compare with MSE

--- tests/util.py
+++ tests/util.py
@@ -1,6 +1,7 @@
 import os
 import hashlib
 
+import numpy as np
 from PIL import Image as PillowImage
 
 from giford.frame_batch import FrameBatch
@@ -23,17 +24,29 @@ def compare_image_files(baseline_filepath: str, test_filepath: str) -> bool:
     assert test_filepath
     assert os.path.exists(test_filepath), "test_filepath does not exist"
 
+    # NO LONGER USING THIS - but keeping here just in case
     # Using PIL/Pillow to compare
     # Ran into issue using file hash since pillow will somtimes compress images when saving
     # which makes the hashes different
     # Hopefully PIL/Pillow is tested enough that this is a reasonable operation
     # NOTE: deleting info dict removes icc_profile, unsure if that matters
-    baseline_pimg = PillowImage.open(baseline_filepath)
-    baseline_pimg.info = {}
-    test_pimg = PillowImage.open(test_filepath)
-    test_pimg.info = {}
+    # baseline_pimg = PillowImage.open(baseline_filepath)
+    # baseline_pimg.info = {}
+    # test_pimg = PillowImage.open(test_filepath)
+    # test_pimg.info = {}
+    #return baseline_pimg == test_pimg
 
-    return baseline_pimg == test_pimg
+    # Using PIL/Pillow to open images since that is indepdent of this project
+    # Calculate MSE of each file
+    # TODO - what is max dfference? don't really know much about this tbh
+    # TODO explore SSIM https://pypi.org/project/SSIM-PIL/
+    baseline_pimg = PillowImage.open(baseline_filepath)
+    test_pimg = PillowImage.open(test_filepath)
+
+    mse = np.square(np.subtract(np.asarray(baseline_pimg), np.asarray(test_pimg))).mean()
+
+    # a MSE is acceptable for equality
+    return mse < 1.00
 
 
 def save_batch_and_compare(
