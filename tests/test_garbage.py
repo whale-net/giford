199091cc30cc368fcc1226b2ee945e2359021dca
qmastermoo <cpsaicontact@gmail.com>
2023-08-19 17:36:30 -0400

    import manifesto

--- tests/test_garbage.py
+++ tests/test_garbage.py
@@ -1,28 +1,29 @@
 import pytest
 
-from giford.image import MultiImage
-from giford.frame import FrameBatch
-from giford.action import Reshape, ReshapeMethod
+# this test uses absoluteish imports for good measure
+import giford
 
 
 def test_feed_garbage_to_multi_image(
-    temp_output_gif: str, orange_swirl_batch: FrameBatch
+    temp_output_gif: str, orange_swirl_batch: giford.frame.FrameBatch
 ):
     # this test should fail
     with pytest.raises(ValueError):
-        new_batch = FrameBatch()
+        new_batch = giford.frame.FrameBatch()
         for i, frame in enumerate(orange_swirl_batch.frames):
             new_batch.add_frame(frame)
 
             if (i % 3) == 0:
-                r = Reshape()
-                temp_batch = FrameBatch()
+                r = giford.action.Reshape()
+                temp_batch = giford.frame.FrameBatch()
                 temp_batch.add_frame(frame).add_frame(frame).add_frame(frame).add_frame(
                     frame
                 )
                 # this should be smaller probably but idk what is going on
-                temp_batch = r.process(temp_batch, ReshapeMethod.RESCALE, 0.25)
+                temp_batch = r.process(
+                    temp_batch, giford.action.ReshapeMethod.RESCALE, 0.25
+                )
                 new_batch.add_batch(temp_batch)
 
-        mimg = MultiImage.create_from_frame_batch(new_batch)
+        mimg = giford.image.MultiImage.create_from_frame_batch(new_batch)
         mimg.save(temp_output_gif)
