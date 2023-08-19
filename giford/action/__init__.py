a397210e8500770fcaba8074615f1d23d235f53c
qmastermoo <cpsaicontact@gmail.com>
2023-08-19 17:03:19 -0400

    sub_module all

--- giford/action/__init__.py
+++ giford/action/__init__.py
@@ -5,3 +5,5 @@ from .scroll import Scroll
 from .shake import Shake
 from .swirl import BasicSwirl, VariableSwirl, VaryingVariableSwirl
 from .translate import Translate
+
+__all__ = [AbstractFrameAction, Reshape, ReshapeMethod, Scroll, Shake, BasicSwirl, VariableSwirl, VaryingVariableSwirl, Translate]
--- giford/frame/__init__.py
+++ giford/frame/__init__.py
@@ -1,2 +1,4 @@
 from .frame_batch import FrameBatch
 from .raw_data import RawDataFrame, RawDataVideo
+
+__all__ = [FrameBatch, RawDataFrame, RawDataVideo]
