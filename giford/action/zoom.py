5995ed357c4691d9907edcfac5765b736cce76c5
qmastermoo <cpsaicontact@gmail.com>
2023-09-12 23:57:43 -0400

    zoom zoom

--- giford/action/zoom.py
+++ giford/action/zoom.py
@@ -6,15 +6,24 @@ from .abstract_frame_action import AbstractFrameAction
 from .crop import Crop
 from .reshape import Reshape, ReshapeMethod
 
-class Crop(AbstractFrameAction):
+
+class Zoom(AbstractFrameAction):
     def __init__(self) -> None:
         super().__init__()
 
     def process(
-        self,
-        input_batch: FrameBatch,
+        self, input_batch: FrameBatch, zoom_step_size_px: int = 0
     ) -> FrameBatch:
-        
+        crop_action = Crop()
+        reshape_action = Reshape(ReshapeMethod.RESIZE)
 
-        
+        output_batch = FrameBatch()
+        for frame in input_batch.frames:
+            temp_batch = FrameBatch.create_from_frame(frame)
+            temp_batch = crop_action.process(temp_batch, crop_px=zoom_step_size_px)
+
+            # TODO aa  parameter
+            temp_batch = reshape_action.process(temp_batch, veritcal_resize_px=frame.height, horiztonal_resize_px=frame.width, enable_anti_aliasing=True)
+            output_batch.add_batch(temp_batch)
+
         return output_batch
---
+++ tests/test_zoom.py
@@ -0,0 +1,32 @@
+import os
+
+import pytest
+
+from giford.frame import FrameBatch
+from giford.action import Zoom
+from tests.util import BASELINE_DIRECTORY, save_batch_and_compare
+
+
+@pytest.mark.parametrize(
+    "zoom_px",
+    [
+        (0),
+        (50),
+        (100),
+        (200)
+    ],
+)
+def test_zoom_basic(temp_output_png: str, orange_image_batch: FrameBatch, zoom_px: int):
+    baseline = os.path.join(
+        BASELINE_DIRECTORY,
+        "test_zoom",
+        f"test_zoom_basic_{zoom_px}.png",
+    )
+
+    z = Zoom()
+
+    output_batch = z.process(orange_image_batch, zoom_step_size_px=zoom_px)
+
+    assert save_batch_and_compare(
+        baseline, output_batch, temp_output_png
+    )
